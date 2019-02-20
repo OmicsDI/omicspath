b01a4e8a547c8f3b21b20d0b1775584cde29533c
Gaurhari <gaurharidass@gmail.com>
2019-02-20 20:15:21 +0000

    adding pathways for datasets in dev db as pathways in additional

--- src/test/scala/org/ebi/ddi/omicspath/service/ReactomeAnalysisServiceTest.scala
+++ src/test/scala/org/ebi/ddi/omicspath/service/ReactomeAnalysisServiceTest.scala
@@ -1,16 +1,42 @@
 package org.ebi.ddi.omicspath.service
 
+import junit.framework.TestCase
+import org.ebi.ddi.omicspath.dbops.MongoOperation
+
 /*
 * @author Gaurhari
 *
 * */
-class ReactomeAnalysisServiceTest {
+class ReactomeAnalysisServiceTest extends TestCase{
 
   var analysisService:ReactomeAnalysisService = _
 
-  def testAnalysis(): Unit ={
+  /*
+  * initialising analysis service
+  * */
+  override def setUp(): Unit = {
+    analysisService = new ReactomeAnalysisService
+  }
 
-    //analysisService.getPathwaysAnalysis()
+  /*
+  * test method to get pathways for datasets
+  * */
+  def testAnalysis(): Unit = {
+
+    for(i <- 0 to MongoOperation.getDatasetsCounts.toInt/100) {
+      val associatedPathways = MongoOperation.getDatasets(i, 100).map(dt => {
+        val pathwaysId = dt.crossReferences.
+          map(cr => analysisService.getPathwaysAnalysis(cr._2.mkString(","), 100, 1))
+          .flatMap(dtr => dtr.pathways.map(dtc => dtc.stId)).toSet
+        println(dt.accession)
+        println(dt.database)
+        MongoOperation.setDatasetPathways(dt.accession, dt.database, pathwaysId)
+      }
+      ).toList
+    }
+    //println(associatedPathways.flatMap(dt => dt.flatMap(dtr => dtr.pathways.map(dtc => dtc.stId))).size)
+
+
   }
 
 }
